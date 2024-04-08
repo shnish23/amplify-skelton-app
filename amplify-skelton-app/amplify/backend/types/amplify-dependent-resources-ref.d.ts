c4366b9796aa4b012731113d33b4043c43836c95
shnish23 <shinichi.nishihata@gmail.com>
2024-04-08 02:42:58 +0000

    create bedrock bot

---
+++ amplify-skelton-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyskeltonappa9a359c5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- amplify-skelton-app/src/routes/+page.svelte
+++ amplify-skelton-app/src/routes/+page.svelte
@@ -1,5 +1,78 @@
 <script lang="ts">
-  import { Avatar } from "@skeletonlabs/skeleton";
+  import { AppBar, Avatar } from "@skeletonlabs/skeleton";
+  import { Amplify } from "aws-amplify";
+  import amplifyconfig from "../amplifyconfiguration.json";
+  Amplify.configure(amplifyconfig);
+
+  import { Hub } from "aws-amplify/utils";
+  import {
+    fetchAuthSession,
+    getCurrentUser,
+    signInWithRedirect,
+    signOut,
+  } from "aws-amplify/auth";
+  import {
+    BedrockRuntimeClient,
+    InvokeModelCommand,
+  } from "@aws-sdk/client-bedrock-runtime";
+
+  Hub.listen("auth", async ({ payload }) => {
+    console.log(payload);
+    switch (payload.event) {
+      case "signInWithRedirect":
+        getUser();
+        break;
+    }
+  });
+
+  function handleSignInClick() {
+    signInWithRedirect();
+  }
+
+  function handleSignOutClick() {
+    signOut();
+  }
+
+  let isAuthorizedUser = false;
+
+  async function getUser() {
+    try {
+      const user = await getCurrentUser();
+      if (user.userId) {
+        isAuthorizedUser = true;
+      }
+    } catch (Exception) {
+      isAuthorizedUser = false;
+    }
+  }
+
+  getUser();
+
+  async function invokeBedrock(prompt: string): Promise<string> {
+    const client = new BedrockRuntimeClient({
+      region: "us-east-1",
+      credentials: (await fetchAuthSession()).credentials,
+    });
+
+    const params = {
+      modelId: "anthropic.claude-instant-v1",
+      contentType: "application/json",
+      accept: "*/*",
+      body: JSON.stringify({
+        prompt: `\n\nHuman: ${prompt}\n\nAssistant:`,
+        max_tokens_to_sample: 1000,
+        temperature: 0.5,
+        top_k: 250,
+        top_p: 1,
+        stop_sequences: ["\n\nHuman:"],
+      }),
+    };
+
+    const command = new InvokeModelCommand(params);
+    const response = await client.send(command);
+
+    return JSON.parse(response.body.transformToString("utf-8"))["completion"];
+  }
 
   let elemChat: HTMLElement;
 
@@ -41,9 +114,8 @@
     messageFeed = [...messageFeed, newMessage];
 
     const tmpMessage = currentMessage;
-    setTimeout(() => {
-      addAiMessage(tmpMessage);
-    }, 1000);
+
+    addAiMessage(tmpMessage);
 
     // Clear prompt
     currentMessage = "";
@@ -54,13 +126,13 @@
     }, 0);
   }
 
-  function addAiMessage(userMessage: string): void {
+  async function addAiMessage(userMessage: string): Promise<void> {
     const newMessage = {
       id: messageFeed.length,
       host: false,
       name: "Claude",
       timestamp: `Today @ ${getCurrentTimestamp()}`,
-      message: `あなたは「${userMessage}」といいました。`,
+      message: await invokeBedrock(userMessage),
       color: "variant-glass-primary",
     };
     // Update the message feed
@@ -81,70 +153,100 @@
   }
 </script>
 
-<div class="h-screen w-full gap-1">
-  <div class="h-full bg-surface-500/30 p-4">
-    <div class="h-full grid grid-rows-[1fr_auto] gap-1">
-      <div class="h-full bg-surface-500/30 p-4 overflow-y-auto">
-        <section
-          bind:this={elemChat}
-          class="h-full p-4 overflow-y-auto space-y-4"
-        >
-          {#each messageFeed as bubble, i}
-            {#if bubble.host === true}
-              <div class="grid grid-cols-[auto_1fr] gap-2">
-                <Avatar
-                  initials="😀"
-                  width="w-12"
-                  fontSize={320}
-                  background={bubble.color}
-                />
-                <div class="card p-4 variant-soft rounded-tl-none space-y-2">
-                  <header class="flex justify-between items-center">
-                    <p class="font-bold">{bubble.name}</p>
-                    <small class="opacity-50">{bubble.timestamp}</small>
-                  </header>
-                  <p>{bubble.message}</p>
-                </div>
-              </div>
-            {:else}
-              <div class="grid grid-cols-[1fr_auto] gap-2">
-                <div class="card p-4 rounded-tr-none space-y-2 {bubble.color}">
-                  <header class="flex justify-between items-center">
-                    <p class="font-bold">{bubble.name}</p>
-                    <small class="opacity-50">{bubble.timestamp}</small>
-                  </header>
-                  <p>{bubble.message}</p>
-                </div>
-                <Avatar
-                  initials="🤖"
-                  width="w-12"
-                  fontSize={320}
-                  background={bubble.color}
-                />
-              </div>
-            {/if}
-          {/each}
-        </section>
-      </div>
-      <div class="bg-surface-500/30 p-4">
-        <div
-          class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token"
-        >
-          <button class="input-group-shim">+</button>
-          <textarea
-            bind:value={currentMessage}
-            class="bg-transparent border-0 ring-0"
-            name="prompt"
-            id="prompt"
-            placeholder="Write a message..."
-            rows="1"
-            on:keydown={onPromptKeydown}
-          />
-          <button class="variant-filled-primary" on:click={addMessage}
-            >Send</button
+<div class="h-dvh w-full gap-1">
+  <div class="h-full grid grid-rows-[auto_1fr]">
+    <AppBar
+      gridColumns="grid-cols-2"
+      slotDefault="place-self-left"
+      slotTrail="place-content-end"
+    >
+      チャットアプリ
+      <svelte:fragment slot="trail">
+        {#if isAuthorizedUser}
+          <button
+            type="button"
+            class="btn btn-sm variant-filled-primary"
+            on:click={handleSignOutClick}>ログアウト</button
           >
+        {:else}
+          <button
+            type="button"
+            class="btn btn-sm variant-filled-primary"
+            on:click={handleSignInClick}>ログイン</button
+          >
+        {/if}
+      </svelte:fragment>
+    </AppBar>
+    <div class="h-full bg-surface-500/30 p-4">
+      {#if isAuthorizedUser}
+        <div class="h-full grid grid-rows-[1fr_auto] gap-1">
+          <div class="h-full bg-surface-500/30 table p-4 overflow-y-auto">
+            <section
+              bind:this={elemChat}
+              class="h-full p-4 overflow-y-auto space-y-4"
+            >
+              {#each messageFeed as bubble, i}
+                {#if bubble.host === true}
+                  <div class="grid grid-cols-[auto_1fr] gap-2">
+                    <Avatar
+                      initials="😀"
+                      width="w-12"
+                      fontSize={320}
+                      background={bubble.color}
+                    />
+                    <div
+                      class="card p-4 rounded-tl-none space-y-2 {bubble.color}"
+                    >
+                      <header class="flex justify-between items-center">
+                        <p class="font-bold">{bubble.name}</p>
+                        <small class="opacity-50">{bubble.timestamp}</small>
+                      </header>
+                      <p>{bubble.message}</p>
+                    </div>
+                  </div>
+                {:else}
+                  <div class="grid grid-cols-[1fr_auto] gap-2">
+                    <div
+                      class="card p-4 rounded-tr-none space-y-2 {bubble.color}"
+                    >
+                      <header class="flex justify-between items-center">
+                        <p class="font-bold">{bubble.name}</p>
+                        <small class="opacity-50">{bubble.timestamp}</small>
+                      </header>
+                      <p>{bubble.message}</p>
+                    </div>
+                    <Avatar
+                      initials="🤖"
+                      width="w-12"
+                      fontSize={320}
+                      background={bubble.color}
+                    />
+                  </div>
+                {/if}
+              {/each}
+            </section>
+          </div>
+          <div class="bg-surface-500/30 p-4">
+            <div
+              class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token"
+            >
+              <button class="input-group-shim">+</button>
+              <textarea
+                bind:value={currentMessage}
+                class="bg-transparent border-0 ring-0"
+                name="prompt"
+                id="prompt"
+                placeholder="Write a message..."
+                rows="1"
+                on:keydown={onPromptKeydown}
+              />
+              <button class="variant-filled-primary" on:click={addMessage}
+                >Send</button
+              >
+            </div>
+          </div>
         </div>
-      </div>
+      {/if}
     </div>
   </div>
 </div>
